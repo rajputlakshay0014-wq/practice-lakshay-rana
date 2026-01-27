58a84627708ecd2c7c3d01c6b58388821839d589
rajputlakshay0014-wq <rajputlakshay0014@gmail.com>
2026-01-27 05:54:58 +0000

    Closure Questions JavaScript

---
+++ 30days.js/Closure.js
@@ -0,0 +1,41 @@
+/* =================================================
+   Closures & Scope Assignment - Practice 2
+   ------------------------------------------------
+   Instructions:
+   - Complete each TODO carefully
+   - Do NOT rename variables or functions
+   - Do NOT change function signatures
+   - Use closures (functions inside functions)
+   - Return exact strings as mentioned
+   ================================================= */
+
+/* TODO 1:
+   Create a global constant named START_COUNT
+   - Set its value to 0
+*/
+
+
+/* TODO 2:
+   Define a function named createCounter
+   - It should accept one parameter:
+     1. step (Number)
+*/
+function createCounter(step) {
+
+  /* TODO 3:
+     Create a local variable named count
+     - Initialize it with START_COUNT
+  */
+
+
+  /* TODO 4:
+     Create an inner function named incrementCounter
+     - It should increase count by step
+     - It should return the updated count
+  */
+
+
+  /* TODO 5:
+     Return the incrementCounter function (closure)
+  */
+}
