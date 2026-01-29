2524da5f7cc5d9b2521884b79c5f75241576ec28
rajputlakshay0014-wq <rajputlakshay0014@gmail.com>
2026-01-29 08:27:29 +0000

    First Programming Questions

---
+++ 30days.js/2day.js
@@ -0,0 +1,120 @@
+/* =================================================
+   Functional Utility Toolkit - PRACTICE (PMF)
+   ------------------------------------------------
+   Instructions for Students:
+   ------------------------------------------------
+   1. DO NOT change function names or parameters
+   2. DO NOT modify the test cases
+   3. DO NOT use console.log
+   4. Do NOT mutate input arrays
+   5. Use ES6 syntax wherever possible
+   6. Use recursion ONLY where mentioned
+   7. Follow clean coding & functional principles
+   ================================================= */
+
+
+/* -------------------------------------------------
+   Task 1: countArray(numbers)
+   - Use recursion
+   - Do NOT use loops (for / while / forEach)
+   - Return total count of elements
+------------------------------------------------- */
+function countArray(numbers) {
+  // TODO: Base case
+  // TODO: Recursive case
+}
+
+
+/* -------------------------------------------------
+   Task 2: addBy(value)
+   - Higher-order function
+   - Return a function
+   - Returned function should add value to input
+------------------------------------------------- */
+function addBy(value) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 3: filterOddNumbers(numbers)
+   - Use functional programming approach
+   - Return only odd numbers
+------------------------------------------------- */
+function filterOddNumbers(numbers) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 4: executeTask(data, task)
+   - task is a function
+   - Demonstrates first-class functions
+------------------------------------------------- */
+function executeTask(data, task) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 5: createToggle()
+   - Must return a function
+   - Use IIFE to create private state
+   - Each call should toggle true/false
+------------------------------------------------- */
+function createToggle() {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 6: doubleNumbers(numbers)
+   - Use anonymous arrow function
+   - Return new array
+   - Do NOT mutate input
+------------------------------------------------- */
+function doubleNumbers(numbers) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 7: isPureMultiply(a, b)
+   - Must be a pure function
+   - Same input → same output
+------------------------------------------------- */
+function isPureMultiply(a, b) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 8: pipe(f, g)
+   - Higher-order function
+   - Return a function
+   - g(f(x))
+------------------------------------------------- */
+function pipe(f, g) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 9: findMin(numbers)
+   - Use recursion
+   - Do NOT use Math.min
+   - Do NOT use loops
+------------------------------------------------- */
+function findMin(numbers) {
+  // TODO
+}
+
+
+/* -------------------------------------------------
+   Task 10: safeModulo(a, b)
+   - Use ES6 default parameters
+   - Return "Invalid operation" if b is 0
+------------------------------------------------- */
+function safeModulo(a = 0, b = 1) {
+  // TODO
+}
